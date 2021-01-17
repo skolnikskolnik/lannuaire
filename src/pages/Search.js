ae2f8c8e2d37645574f5b7bee8b8c0e474982da2
Julie Altman <jtskolnik@gmail.com>
2021-01-17 13:57:27 -0500

    user can sort alphabetically byclicking a button

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,51 +9,55 @@ import Button from "../components/Button"
 //I want the json object available as a prop
 let randList;
 class Search extends Component {
-  state = {
-    result: [],
-    search: "",
-    countryCode: ""
-  };
+    state = {
+        result: [],
+        search: "",
+        countryCode: "",
+        alphebetized: false
+    };
 
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getRandomList()
-        .then(res => {
-            randList = res.data.results;
-            this.setState({result: randList})
-        });
-  }
-
-
-  //Search by state
-  searchByCity = (query) => {
-    API.getRandomNational(query)
-        .then(res => this.setState({result: res}))
-        .catch(err => console.log(err))
-  }
+    // When the component mounts, get a list of all available base breeds and update this.state.breeds
+    componentDidMount() {
+        API.getRandomList()
+            .then(res => {
+                randList = res.data.results;
+                this.setState({ result: randList })
+            });
+    }
 
 
-  //The user should be able to sort by last name and gender
-  render() {
-    return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Your employees:</h1>
-            <Button text={"Sort alphabetically"} name={"alphabet"}/>
-            <Button text={"Show only men"} name={"men"} />
-            <Button text={"Show only women"} name={"women"} />
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchResults results={this.state.results} />
-        </Container>
-        <Table randList={this.state.result}/>
-      </div>
-    );
-  }
+    sortAlphabet = () => {
+        //This takes randList and orders it alphabetically
+        randList.sort((a, b) => (a.name.last > b.name.last) ? 1 : -1);
+
+        //Now we need to repopulate the table using the new randList
+        //First step, change alphebetized to true
+        this.setState({ alphebetized: true });
+        console.log(randList);
+    }
+
+
+    //The user should be able to sort by last name and gender
+    render() {
+        return (
+            <div>
+                <Container style={{ minHeight: "80%" }}>
+                    <h1 className="text-center">Your employees:</h1>
+                    <Button cb={this.sortAlphabet} text={"Sort alphabetically"} name={"alphabet"} />
+                    <Button text={"Show only men"} name={"men"} />
+                    <Button text={"Show only women"} name={"women"} />
+                    <Alert
+                        type="danger"
+                        style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+                    >
+                        {this.state.error}
+                    </Alert>
+                    <SearchResults results={this.state.results} />
+                </Container>
+                <Table sortAlphabet={this.sortAlphabet} randList={this.state.result} />
+            </div>
+        );
+    }
 }
 
 export default Search;
